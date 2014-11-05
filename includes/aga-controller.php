e202f8cef8bb0d147a4e21c6a790ba7e972c5f9b
Ryan Kienstra <kienstraryan@gmail.com>
2014-11-05 09:44:59 -0800

    whitespace removed

--- includes/aga-controller.php
+++ includes/aga-controller.php
@@ -1,16 +1,16 @@
 <?php
 
-// Append form to bottom of post	 
-add_action( 'template_redirect' , 'aga_search_for_form_to_display_at_end_of_post' ) ; 
-function aga_search_for_form_to_display_at_end_of_post() { 
+// Append form to bottom of post
+add_action( 'template_redirect' , 'aga_search_for_form_to_display_at_end_of_post' ) ;
+function aga_search_for_form_to_display_at_end_of_post() {
 	$forms = RGFormsModel::get_forms( null, 'title' ) ;
 	aga_manage_form_options( $forms ) ;
 }
 
-function aga_manage_form_options( $forms ) { 
+function aga_manage_form_options( $forms ) {
 	foreach( $forms as $form ) {
-		aga_maybe_append_form_to_end_of_post( $form ) ;
-		aga_maybe_display_form_horizontally( $form ) ;
+	       aga_maybe_append_form_to_end_of_post( $form ) ;
+	       aga_maybe_display_form_horizontally( $form ) ;
 	}
 }
 
@@ -23,13 +23,13 @@ function aga_maybe_append_form_to_end_of_post( $form ) {
 
 function aga_do_append_form_to_end_of_post( $form_id ) {
 	$form = GFAPI::get_form( $form_id ) ;
-	return ( ( isset( $form[ 'aga_bottom_of_post' ] ) ) && ( '1' == $form[ 'aga_bottom_of_post' ] ) ) ; 
+	return ( ( isset( $form[ 'aga_bottom_of_post' ] ) ) && ( '1' == $form[ 'aga_bottom_of_post' ] ) ) ;
 }
-	
+
 function aga_append_form_to_end_of_single_post_page( $form_id ) {
 	if ( aga_is_page_a_single_post() ) {
-		AGA_Form::add_form( $form_id ) ; 
-		add_filter( 'the_content' , array( 'AGA_Form' , 'append_form_to_content' ) , '100' ) ; 
+		AGA_Form::add_form( $form_id ) ;
+		add_filter( 'the_content' , array( 'AGA_Form' , 'append_form_to_content' ) , '100' ) ;
 	}
 }
 
@@ -41,13 +41,13 @@ function aga_maybe_display_form_horizontally( $form ) {
 
 function aga_do_display_horizontally( $form_id ) {
 	$form = GFAPI::get_form( $form_id ) ;
-	return ( ( isset( $form[ 'aga_horizontal_display' ] ) ) && ( '1' == $form[ 'aga_horizontal_display' ] ) ) ; 
+	return ( ( isset( $form[ 'aga_horizontal_display' ] ) ) && ( '1' == $form[ 'aga_horizontal_display' ] ) ) ;
 }
 
 function aga_display_form_horizontally( $form_id ) {
 	$form = GFAPI::get_form( $form_id ) ;
 	$form_with_horizontal_display = aga_add_horizontal_display( $form ) ;
-	GFAPI::update_form( $form_with_horizontal_display , $form_id ) ;	
+	GFAPI::update_form( $form_with_horizontal_display , $form_id ) ;
 }
 
 function aga_add_horizontal_display( $form ) {
@@ -57,11 +57,11 @@ function aga_add_horizontal_display( $form ) {
 	else if ( aga_form_has_classes_but_not_an_inline_class( $form ) ) {
 		$form[ 'cssClass' ] = $form[ 'cssClass' ] . ' gform_inline' ;
 	}
-	return $form ; 
+	return $form ;
 }
 
 function aga_form_does_not_have_any_classes( $form ) {
-	return ( ( isset( $form[ 'cssClass' ] ) && ( "" == $form[ 'cssClass' ] ) ) ) ; 
+	return ( ( isset( $form[ 'cssClass' ] ) && ( "" == $form[ 'cssClass' ] ) ) ) ;
 }
 
 function aga_form_has_classes_but_not_an_inline_class( $form ) {
@@ -70,27 +70,27 @@ function aga_form_has_classes_but_not_an_inline_class( $form ) {
 
 function aga_is_page_a_single_post() {
 	global $post ;
-	return ( isset( $post ) && is_single() && ( 'post' == $post->post_type ) ) ; 
+	return ( isset( $post ) && is_single() && ( 'post' == $post->post_type ) ) ;
 }
 
 
 // Placeholders instead of labels
 add_filter( 'gform_field_content' , 'aga_maybe_insert_placeholders_and_remove_labels' , 11 , 5 ) ;
 function aga_maybe_insert_placeholders_and_remove_labels( $content, $field, $value, $lead_id, $form_id ) {
-	if ( is_form_set_to_show_aga_placeholder( $form_id ) ) { 
+	if ( is_form_set_to_show_aga_placeholder( $form_id ) ) {
 		$placeholder = $field[ 'label' ] ;
-		$content = aga_get_content_with_placeholder_and_without_label( $content , $placeholder ) ;		
+		$content = aga_get_content_with_placeholder_and_without_label( $content , $placeholder ) ;
 	}
-	return $content ; 
+	return $content ;
 }
 
 function is_form_set_to_show_aga_placeholder( $form_id ) {
 	$form = GFAPI::get_form( $form_id ) ;
-	return ( ( isset( $form[ 'labelPlacement' ] ) ) && ( 'in_placeholder' == $form[ 'labelPlacement' ] ) ) ; 
+	return ( ( isset( $form[ 'labelPlacement' ] ) ) && ( 'in_placeholder' == $form[ 'labelPlacement' ] ) ) ;
 }
 
 function aga_get_content_with_placeholder_and_without_label( $content , $placeholder ) {
-	$content_with_placeholder = preg_replace( "/(<input[^>]*?type=\'(text|email)\')/" , "$1 placeholder='$placeholder'" , $content ) ; 
+	$content_with_placeholder = preg_replace( "/(<input[^>]*?type=\'(text|email)\')/" , "$1 placeholder='$placeholder'" , $content ) ;
 	$content_with_placeholder_and_without_label = preg_replace( "/<label.*?<\/label>/" , "" , $content_with_placeholder ) ;
 	return $content_with_placeholder_and_without_label ;
 }
@@ -98,9 +98,9 @@ function aga_get_content_with_placeholder_and_without_label( $content , $placeho
 
 add_filter( 'gform_field_content' , 'aga_set_class_of_input_tags' , 12 , 5 ) ;
 function aga_set_class_of_input_tags( $content, $field, $value, $lead_id, $form_id ) {
-	$new_class = apply_filters( 'aga_gravity_form_input_class' , 'form-control' , $form_id ) ; 
-	$new_content = aga_add_class_to_input( $content , $new_class ) ;
-	return $new_content ; 
+	$new_class = apply_filters( 'aga_gravity_form_input_class' , 'form-control' , $form_id ) ;
+	$new_content = aga_add_class_to_input( $content , esc_attr( $new_class ) );
+	return $new_content ;
 }
 
 function aga_add_class_to_input( $content , $new_class ) {
@@ -113,14 +113,14 @@ function aga_add_class_to_input( $content , $new_class ) {
 add_filter( 'gform_submit_button' , 'aga_submit_button' , 10 , 2 ) ;
 function aga_submit_button( $button_input , $form ) {
 	$new_classes = apply_filters( 'aga_submit_button_classes' , 'btn btn-primary btn-med' , $form ) ;
-	$class_attribute = "class='" ;		
+	$class_attribute = "class='" ;
 	if ( false !== strpos( $button_input , $class_attribute ) ) {
-		$class_attribute_with_new_classes = $class_attribute . $new_classes . ' ' ;		
+		$class_attribute_with_new_classes = $class_attribute . $new_classes . ' ' ;
 		$filtered_button =	str_replace( $class_attribute , $class_attribute_with_new_classes , $button_input ) ;
-		return $filtered_button ;		 
+		return $filtered_button ;
 	} else {
 		$opening_input = '<input' ;
-		$input_with_new_classes = $opening_input . " class='{$new_classes}'" ; 
+		$input_with_new_classes = $opening_input . " class='{$new_classes}'" ;
 		$filtered_button = str_replace( $opening_input , $input_with_new_classes , $button_input ) ;
 		return $filtered_button ;
 	}
