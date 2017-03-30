263ad2164e68a67bb65f03878119c45bff0c5fa3
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-03-29 23:41:06 -0300

    Add PHP Docblocks to class Gravity_Settings.

--- php/class-gravity-settings.php
+++ php/class-gravity-settings.php
@@ -7,6 +7,9 @@
 
 namespace AdapterGravityAddOn;
 
+/**
+ * Class Gravity_Settings
+ */
 class Gravity_Settings {
 
 	/**
@@ -18,7 +21,16 @@ class Gravity_Settings {
 		add_filter( 'gform_pre_form_settings_save' , array( $this, 'save_aga_settings' ) );
 	}
 
-	// Option to output label as placeholder.
+	/**
+	 * Add an option to output the label as a placeholder.
+	 *
+	 * For example, instead of placing 'Email' under an <input> as a label,
+	 * output it as the placeholder value of the <input>.
+	 *
+	 * @param array  $settings Associated with forms.
+	 * @param object $form The form object that is shown.
+	 * @return array $settings Filtered to conditionally add label placement.
+	 */
 	function gform_add_label_option( $settings, $form ) {
 		$form_label_placement = isset( $settings['Form Layout']['form_label_placement'] ) ? $settings['Form Layout']['form_label_placement'] : '';
 		if ( $form_label_placement ) {
@@ -27,6 +39,17 @@ class Gravity_Settings {
 		return $settings;
 	}
 
+	/**
+	 * Add an option to output the label as a placeholder.
+	 *
+	 * For example, instead of placing 'Email' under an <input> as a label,
+	 * output it as the placeholder value of the <input>.
+	 *
+	 * @param string $form_label_placement Where to display the label.
+	 * @param array  $settings Associated with forms.
+	 * @param object $form The form object that is shown.
+	 * @return array $settings Associated with forms.
+	 */
 	function add_label_placement_to_settings( $form_label_placement, $settings, $form ) {
 		$new_option = $this->get_option_for_in_placeholder( $form );
 		$new_form_label_settings = $this->get_new_form_label_settings( $new_option , $form_label_placement );
@@ -34,6 +57,12 @@ class Gravity_Settings {
 		return $settings;
 	}
 
+	/**
+	 * Get the <option> element for whether to output the label as a placeholder.
+	 *
+	 * @param object $form The form object that is shown.
+	 * @return string $markup <option> element.
+	 */
 	function get_option_for_in_placeholder( $form ) {
 		$option_name = 'in_placeholder';
 		$selected_attribute = $this->get_placeholder_selected_attribute( $form );
@@ -42,6 +71,13 @@ class Gravity_Settings {
 						. '</option>';
 	}
 
+	/**
+	 * Get new markup for form label settings.
+	 *
+	 * @param string $new_option Element with an option for the form.
+	 * @param string $form_label_placement Where to display the label.
+	 * @return string $new_settings.
+	 */
 	function get_new_form_label_settings( $new_option, $form_label_placement ) {
 		$closing_select_tag = '</select>';
 		$new_option_with_closing_select_tag = $new_option . ' ' . $closing_select_tag;
@@ -60,13 +96,25 @@ class Gravity_Settings {
 		return $selected_attribute;
 	}
 
-	// Option to echo form at bottom of post and to display it inline
+	/**
+	 * Get the form settings, with additional options.
+	 *
+	 * @param array  $settings Associated with forms.
+	 * @param object $form The form object that is shown.
+	 * @return array  $settings Now with options to place the label inline and at the bottom.
+	 */
 	function gform_add_settings( $settings, $form ) {
-		$settings_with_bottom_of_post_option = Bottom_Of_Post_Setting::get_settings( $settings , $form );
+		$settings_with_bottom_of_post_option = Bottom_Of_Post_Setting::get_settings( $settings, $form );
 		$settings_with_inline_and_bottom_of_post_options = Horizontal_Form_Setting::get_settings( $settings_with_bottom_of_post_option , $form );
 		return $settings_with_inline_and_bottom_of_post_options;
 	}
 
+	/**
+	 * Get the form settings, with additional options.
+	 *
+	 * @param object $form The form object that is shown.
+	 * @return object $form With additional settings.
+	 */
 	function save_aga_settings( $form ) {
 		$form['aga_bottom_of_post'] = rgpost( 'aga_bottom_of_post' );
 		$form['aga_horizontal_display'] = rgpost( 'aga_horizontal_display' );
