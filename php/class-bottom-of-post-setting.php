fdb6bded026f30db84940b48ba3a6571b0b64a20
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-03-30 00:53:49 -0300

    Add docblocks to two subclasses.
    These extend the same class, and are similar.
    Also, add an indentation in the arrays.

--- php/class-bottom-of-post-setting.php
+++ php/class-bottom-of-post-setting.php
@@ -12,11 +12,18 @@ namespace AdapterGravityAddOn;
  */
 class Bottom_Of_Post_Setting extends AGA_Setting {
 
+	/**
+	 * Get settings with new markup for the bottom of the post.
+	 *
+	 * @param array  $settings Gravity Form settings.
+	 * @param object $form Data for Gravity Form.
+	 * @return array $settings Filtered settings.
+	 */
 	public static function get_settings( $settings, $form ) {
 		parent::instantiate( $settings , $form );
 		parent::set_variables( array(
 			'setting_name' => 'aga_bottom_of_post',
-				'setting_description' => __( 'Display at the bottom of every single-post page' , 'adapter-gravity-add-on' ),
+			'setting_description' => __( 'Display at the bottom of every single-post page' , 'adapter-gravity-add-on' ),
 		));
 		return parent::$instance->settings_with_new_markup();
 	}
--- php/class-horizontal-form-setting.php
+++ php/class-horizontal-form-setting.php
@@ -12,12 +12,19 @@ namespace AdapterGravityAddOn;
  */
 class Horizontal_Form_Setting extends AGA_Setting {
 
+	/**
+	 * Get settings with new markup for the horizontal form.
+	 *
+	 * @param array  $settings Gravity Form settings.
+	 * @param object $form Data for Gravity Form.
+	 * @return array $settings Filtered settings.
+	 */
 	public static function get_settings( $settings, $form ) {
-		parent::instantiate( $settings , $form );
+		parent::instantiate( $settings, $form );
 		parent::set_variables( array(
 			'setting_name' => 'aga_horizontal_display',
-					'setting_description' => __( 'Display form horizontally' , 'adapter-gravity-add-on' ),
-	 	) );
+			'setting_description' => __( 'Display form horizontally' , 'adapter-gravity-add-on' ),
+		) );
 		return parent::$instance->settings_with_new_markup();
 	}
 }
