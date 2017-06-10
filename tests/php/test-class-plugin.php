9553af3436b615d02954e4399a6a55c49508b806
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-06-10 18:39:00 -0500

    Create PHPUnit tests for class Plugin.
    Store the plugin instance in a property.
    And ensure that the registration occurs when Gravity Forms is included.

---
+++ tests/php/test-class-plugin.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Tests for class Plugin.
+ *
+ * @package AdapterGravityAddOn
+ */
+
+namespace AdapterGravityAddOn;
+
+/**
+ * Tests for class Plugin.
+ *
+ * @package AdapterGravityAddOn
+ */
+class Test_Plugin extends \WP_UnitTestCase {
+
+	/**
+	 * Instance of Plugin.
+	 *
+	 * @var object.
+	 */
+	public $instance;
+
+	/**
+	 * Setup.
+	 *
+	 * @inheritdoc
+	 */
+	public function setUp() {
+		parent::setUp();
+		include_once( dirname( __FILE__ ) . '/../../adapter-gravity-add-on.php' );
+		$this->instance = Plugin::get_instance();
+	}
+
+	/**
+	 * Test get_instance().
+	 *
+	 * @see Plugin::get_instance().
+	 */
+	public function test_get_instance() {
+		$this->assertEquals( 'AdapterGravityAddOn\Plugin', get_class( $this->instance ) );
+	}
+
+	/**
+	 * Test construct().
+	 *
+	 * @see Plugin::__construct().
+	 */
+	public function test_construct() {
+		$this->assertEquals( 5, has_action( 'gform_loaded', array( $this->instance, 'register' ) ) );
+	}
+
+	/**
+	 * Test register().
+	 *
+	 * @see Plugin::register().
+	 */
+	public function test_register() {
+		$this->instance->register();
+		// Gravity Forms isn't yet included, so the file inclusion shouldn't take place.
+		$this->assertFalse( class_exists( 'AdapterGravityAddOn\Adapter_Add_On' ) );
+
+		require_once( dirname( __FILE__ ) . '/../../../gravityforms/gravityforms.php' );
+		$this->instance->register();
+		$this->assertTrue( class_exists( 'AdapterGravityAddOn\Adapter_Add_On' ) );
+		$this->assertTrue( class_exists( '\GFAddOn' ) );
+		$this->assertTrue( in_array( 'AdapterGravityAddOn\Adapter_Add_On', \GFAddOn::get_registered_addons(), true ) );
+	}
+
+}
