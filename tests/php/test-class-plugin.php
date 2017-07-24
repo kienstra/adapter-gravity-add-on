577c9acb60e7f7b67c5ff0a5a1614af22101a0e8
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-07-23 21:43:30 -0500

    PHPUnit tests for class Plugin.
    Add assertions for test_construct() and test_register().
    Also, add include_once() calls in setUp().

--- tests/php/test-class-plugin.php
+++ tests/php/test-class-plugin.php
@@ -28,8 +28,6 @@ class Test_Plugin extends \WP_UnitTestCase {
 	 */
 	public function setUp() {
 		parent::setUp();
-		include_once( dirname( __FILE__ ) . '/../../../gravityforms/gravityforms.php' );
-		include_once( dirname( __FILE__ ) . '/../../adapter-gravity-add-on.php' );
 		$this->instance = Plugin::get_instance();
 	}
 
@@ -39,16 +37,8 @@ class Test_Plugin extends \WP_UnitTestCase {
 	 * @see Plugin::get_instance().
 	 */
 	public function test_get_instance() {
-		$this->assertEquals( 'AdapterGravityAddOn\Plugin', get_class( $this->instance ) );
-	}
-
-	/**
-	 * Test construct().
-	 *
-	 * @see Plugin::__construct().
-	 */
-	public function test_construct() {
-		$this->assertEquals( 5, has_action( 'gform_loaded', array( $this->instance, 'register' ) ) );
+		$this->assertEquals( 'AdapterGravityAddOn\Plugin', get_class( Plugin::get_instance() ) );
+		$this->assertEquals( $this->instance, Plugin::get_instance() );
 	}
 
 	/**
@@ -57,7 +47,6 @@ class Test_Plugin extends \WP_UnitTestCase {
 	 * @see Plugin::register().
 	 */
 	public function test_register() {
-		$this->instance->register();
 		$this->assertTrue( class_exists( 'AdapterGravityAddOn\Adapter_Add_On' ) );
 		$this->assertTrue( class_exists( '\GFAddOn' ) );
 		$this->assertTrue( in_array( 'AdapterGravityAddOn\Adapter_Add_On', \GFAddOn::get_registered_addons(), true ) );
