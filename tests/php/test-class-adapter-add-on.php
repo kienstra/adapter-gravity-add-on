78a836cccdbbd6da4b617dc0c3705e49bac2a10c
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-07-30 14:00:30 -0500

    Add PHPUnit tests for class-adapter-add-on.php.
    Including for load_plugin_files and plugin_textdomain.
    @todo: finish PHPUnit tests for this file.

--- tests/php/test-class-adapter-add-on.php
+++ tests/php/test-class-adapter-add-on.php
@@ -36,6 +36,26 @@ class Test_Class_Adapter_Add_On extends Test_Adapter_Gravity_Add_On {
 	}
 
 	/**
+	 * Test get_instance().
+	 *
+	 * @see Adapter_Add_Onn::get_instance().
+	 */
+	public function test_get_instance() {
+		$this->assertEquals( 'AdapterGravityAddOn\Adapter_Add_On', get_class( Adapter_Add_On::get_instance() ) );
+		$this->assertEquals( $this->instance, Adapter_Add_On::get_instance() );
+	}
+
+	/**
+	 * Test pre_init().
+	 *
+	 * @see Adapter_Add_Onn::pre_init().
+	 */
+	public function test_pre_init() {
+		$this->assertEquals( 'Adapter Gravity Add On', $this->instance->_title );
+		$this->assertEquals( 'Adapter Add On', $this->instance->_short_title );
+	}
+
+	/**
 	 * Test construct().
 	 *
 	 * @see Adapter_Add_Onn::__construct().
@@ -49,6 +69,28 @@ class Test_Class_Adapter_Add_On extends Test_Adapter_Gravity_Add_On {
 		$this->assertInternalType( 'string', $this->instance->_title );
 		$this->assertInternalType( 'string', $this->instance->_short_title );
 		$this->assertEquals( true, $this->instance->do_enqueue_plugin_styling_by_default );
+	}
+
+	/**
+	 * Test load_plugin_files().
+	 *
+	 * @see Adapter_Add_Onn::load_plugin_files().
+	 */
+	public function test_load_plugin_files() {
+		$this->instance->load_plugin_files();
+		$this->assertTrue( class_exists( 'AdapterGravityAddOn\Layout_Setting' ) );
+		$this->assertTrue( class_exists( 'AdapterGravityAddOn\Gravity_Setting' ) );
+		$this->assertTrue( class_exists( 'AdapterGravityAddOn\Email_Form' ) );
+	}
+
+	/**
+	 * Test plugin_textdomain().
+	 *
+	 * @see Adapter_Add_Onn::plugin_textdomain().
+	 */
+	public function test_plugin_textdomain() {
+		$this->instance->init();
+		$this->assertEquals( 10, has_action( 'init', array( $this->instance, 'plugin_textdomain' ) ) );
 	}
 
 }
