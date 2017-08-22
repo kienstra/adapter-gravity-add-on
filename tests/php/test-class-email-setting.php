91a185d61c2264bc39b55b69d6b618440388f8f7
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-08-22 18:52:25 -0500

    AGA-1 : PHPUnit tests for 2 functions in Email_Setting.
    Test get_bottom_of_post_setting() and get_horizontal_setting().
    These are very similar tests, and might benefit from code reuse.

--- tests/php/test-class-email-setting.php
+++ tests/php/test-class-email-setting.php
@@ -64,4 +64,60 @@ class Test_Class_Email_Setting extends Test_Adapter_Gravity_Add_On {
 		$this->assertEquals( 10, has_filter( 'gform_pre_form_settings_save', array( $this->instance, 'save_settings' ) ) );
 	}
 
+	/**
+	 * Test test_get_bottom_of_post_setting().
+	 *
+	 * @see Email_Setting::get_bottom_of_post_setting().
+	 */
+	public function test_get_bottom_of_post_setting() {
+		$layout_key = 'Form Layout';
+		$initial_settings = array(
+			$layout_key => array(),
+		);
+		$form = array(
+			$this->instance->bottom_of_post => '1',
+		);
+		$actual_settings = $this->instance->get_bottom_of_post_setting( $initial_settings, $form );
+		$markup = $actual_settings[ $layout_key ][ $this->instance->bottom_of_post ];
+
+		$this->assertContains( $this->instance->bottom_of_post, $markup );
+		$this->assertContains( 'Display at the bottom of every single-post page', $markup );
+		$this->assertContains( 'checked=\'checked\'', $markup );
+
+		$form_not_checked = array(
+			$this->instance->bottom_of_post => '',
+		);
+		$actual_settings_not_checked = $this->instance->get_bottom_of_post_setting( $initial_settings, $form_not_checked );
+		$markup_not_checked = $actual_settings_not_checked[ $layout_key ][ $this->instance->bottom_of_post ];
+		$this->assertNotContains( 'checked=\'checked\'', $markup_not_checked );
+	}
+
+	/**
+	 * Test test_get_horizontal_setting().
+	 *
+	 * @see Email_Setting::get_get_horizontal_setting().
+	 */
+	public function test_get_horizontal_setting() {
+		$layout_key = 'Form Layout';
+		$initial_settings = array(
+			$layout_key => array(),
+		);
+		$form = array(
+			$this->instance->horizontal_display => '1',
+		);
+		$actual_settings = $this->instance->get_horizontal_setting( $initial_settings, $form );
+		$markup = $actual_settings[ $layout_key ][ $this->instance->horizontal_display ];
+
+		$this->assertContains( $this->instance->horizontal_display, $markup );
+		$this->assertContains( 'Display form horizontally', $markup );
+		$this->assertContains( 'checked=\'checked\'', $markup );
+
+		$form_not_checked = array(
+			$this->instance->horizontal_display => '',
+		);
+		$actual_settings_not_checked = $this->instance->get_bottom_of_post_setting( $initial_settings, $form_not_checked );
+		$markup_not_checked = $actual_settings_not_checked[ $layout_key ][ $this->instance->bottom_of_post ];
+		$this->assertNotContains( 'checked=\'checked\'', $markup_not_checked );
+	}
+
 }
