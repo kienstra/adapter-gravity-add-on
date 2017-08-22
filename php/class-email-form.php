523a749c12c48bcd500fc8ccdee5fc0af1aee716
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-08-22 15:46:51 -0500

    AGA-1 : Only display class on in front-end markup.
    Before, this appeared in the back-end <input> field 'CSS Class Name.'
    But the user doesn't need to see the class.
    And it's unexpected to see it display on checking 'Display form horizontally.'

--- php/class-email-form.php
+++ php/class-email-form.php
@@ -47,7 +47,7 @@ class Email_Form {
 	 */
 	public function __construct( $plugin ) {
 		$this->plugin = $plugin;
-		add_filter( 'gform_pre_form_settings_save', array( $this, 'conditionally_display_form_horizontally' ) );
+		add_filter( 'gform_pre_render', array( $this, 'conditionally_display_form_horizontally' ) );
 		add_filter( 'the_content', array( $this, 'conditionally_append_form' ), 100 );
 		add_filter( 'gform_field_content', array( $this, 'set_class_of_input_tags' ), 12, 5 );
 		add_filter( 'gform_submit_button', array( $this, 'submit_button' ), 10, 2 );
