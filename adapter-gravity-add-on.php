9e0e56665d4d63c5f152af9a82b92c38e09ab9e9
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-08-24 15:00:00 -0500

    AGA-1 : Make plugin description shorter.
    In adapter-gravity-add-on.php,
    Remove references to placeholders, as this featured is removed.

--- adapter-gravity-add-on.php
+++ adapter-gravity-add-on.php
@@ -10,7 +10,7 @@ namespace AdapterGravityAddOn;
 /*
 Plugin Name: Adapter Gravity Add-On
 Plugin URI: http://ryankienstra.com/adapter-gravity-add-on
-Description: Add-on for Gravity Forms, with options for inline display, and showing at the end of every post. To use, click "Forms" in the left menu of your admin screen. Scroll over one of your forms, and click "Form Settings." Scroll down to "Form Layout." You'll see a new option for "Label placement": "In placeholder." You'll also see options to "Display at the bottom of every single-post page" and "Display form horizontally."
+Description: Add-on for Gravity Forms, with options for inline display, and showing at the end of every post. To use, click "Forms" in the left menu of your admin screen. Scroll over one of your forms, and click "Form Settings." Scroll down to "Form Layout." You'll see options to "Display at the bottom of every single-post page" and "Display form horizontally."
 
 Version: 1.0.2
 Author: Ryan Kienstra
