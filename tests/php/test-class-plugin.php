c3ca09d9690cdb904e22e5f2c31af0f55b4af991
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-08-25 16:21:46 -0500

    AGA-1 : PHPUnit tests for Plugin::__construct().
    Test that it has the right action.
    And that its property is correct.

--- tests/php/test-class-plugin.php
+++ tests/php/test-class-plugin.php
@@ -42,14 +42,37 @@ class Test_Plugin extends Test_Adapter_Gravity_Add_On {
 	}
 
 	/**
+	 * Test __construct().
+	 *
+	 * @see Plugin::__construct().
+	 */
+	public function test_construct() {
+		$this->assertEquals( 5, has_action( 'gform_loaded', array( $this->instance, 'register' ) ) );
+		$this->assertEquals( '1.9', $this->instance->_min_gravityforms_version );
+	}
+
+	/**
 	 * Test register().
 	 *
 	 * @see Plugin::register().
 	 */
 	public function test_register() {
+		$this->instance->register();
 		$this->assertTrue( class_exists( 'AdapterGravityAddOn\Adapter_Add_On' ) );
 		$this->assertTrue( class_exists( '\GFAddOn' ) );
 		$this->assertTrue( in_array( 'AdapterGravityAddOn\Adapter_Add_On', \GFAddOn::get_registered_addons(), true ) );
 	}
 
+	/**
+	 * Test gravity_not_installed().
+	 *
+	 * @see Plugin::gravity_not_installed().
+	 */
+	public function test_gravity_not_installed() {
+		ob_start();
+		$this->instance->gravity_not_installed();
+		$buffer = ob_get_clean();
+		$this->assertContains( 'In order to use the Adapter Gravity Add-On plugin', $buffer );
+		$this->assertContains( $this->instance->_min_gravityforms_version, $buffer );
+	}
 }
