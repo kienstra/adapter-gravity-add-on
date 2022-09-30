ed4474ab4bee28c9bb5f2694afc461c0ea412afd
Ryan Kienstra <kienstraryan@gmail.com>
2022-09-29 21:16:06 -0500

    Update the version and Stable tag to 1.0.3

--- adapter-gravity-add-on.php
+++ adapter-gravity-add-on.php
@@ -7,15 +7,15 @@
 
 namespace AdapterGravityAddOn;
 
-use GFForms;
 use GFAddOn;
+use GFForms;
 
 /*
 Plugin Name: Adapter Gravity Add-On
 Plugin URI: http://ryankienstra.com/adapter-gravity-add-on
 Description: Add-on for Gravity Forms, with options for inline display, and showing at the end of every post. To use, click "Forms" in the left menu of your admin screen. Scroll over one of your forms, and click "Form Settings." Scroll down to "Form Layout." You'll see options to "Display at the bottom of every single-post page" and "Display form horizontally."
 
-Version: 1.0.2
+Version: 1.0.3
 Author: Ryan Kienstra
 Author URI: http://ryankienstra.com
 License: GPL2+
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: forms, styling, gravity
 Requires at least: 3.8
 Requires PHP: 8.0
 Tested up to: 6.0
-Stable tag: 1.0.2
+Stable tag: 1.0.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
