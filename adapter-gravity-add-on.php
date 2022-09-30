d5736d34be4708255ea708a711d8ebac8bba53d4
Ryan Kienstra <kienstraryan@gmail.com>
2022-09-29 21:24:36 -0500

    Update the plugin URI to be the GitHub URL

--- adapter-gravity-add-on.php
+++ adapter-gravity-add-on.php
@@ -12,11 +12,11 @@ use GFForms;
 
 /*
 Plugin Name: Adapter Gravity Add-On
-Plugin URI: http://ryankienstra.com/adapter-gravity-add-on
+Plugin URI: https://github.com/kienstra/adapter-gravity-add-on
 Description: Add-on for Gravity Forms, with an option to show a form at the end of every post. To use, click "Forms" in the left menu of your admin screen. Scroll over one of your forms, and click "Settings." Scroll down to "Form Options." You'll see a toggle "Display at the bottom of every post."
 Version: 1.0.3
 Author: Ryan Kienstra
-Author URI: http://ryankienstra.com
+Author URI: https://getlocalci.com
 License: GPL2+
 */
 
