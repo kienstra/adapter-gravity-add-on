99fd64b741a6a111963959ff81d9f9388620c131
Ryan Kienstra <kienstraryan@gmail.com>
2024-04-16 23:45:08 -0600

    Remove needless 'Tested up to' from .php file

--- adapter-gravity-add-on.php
+++ adapter-gravity-add-on.php
@@ -9,11 +9,10 @@
  * Description: Add-on for Gravity Forms, with an option to show a form at the end of every post. To use, click "Forms" in the left menu of your admin screen. Scroll over one of your forms, and click "Settings." Scroll down to "Form Options." You'll see a toggle "Display at the bottom of every post."
  * Version: 2.0.0
  * Requires at least: 6.4
- * Tested up to: 6.5
  * Requires PHP: 8.0
+ * Requires Plugins: gravityforms
  * Author: Ryan Kienstra
  * Author URI: https://ryankienstra.com
- * Requires Plugins: gravityforms
  * License: GPL2 v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: adapter-gravity-add-on
